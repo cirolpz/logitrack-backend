b7ee7098ed571c088be3b0cd53163ef7466bfcb6
KarinPellegrini <karinfarnholc@gmail.com>
2026-04-04 00:03:19 -0300

    fix: actualizar CORS para frontend en Vercel

--- src/main/java/com/logitrack/logitrack_api/controller/EnvioController.java
+++ src/main/java/com/logitrack/logitrack_api/controller/EnvioController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/envios")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://logitrack-prototype.vercel.app")
 public class EnvioController {
     private final EnvioService service;
     // http://localhost:8080/swagger-ui/index.html
